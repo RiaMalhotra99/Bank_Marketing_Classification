a338fa0bc9bb036a78bfb4d3235a0b3a7a2e2073
RiaMalhotra99 <ria.malhotra1448@gmail.com>
2026-01-11 15:37:46 +0530

    Update kNN.py

--- model/kNN.py
+++ model/kNN.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.metrics import f1_score, roc_auc_score, matthews_corrcoef
 
